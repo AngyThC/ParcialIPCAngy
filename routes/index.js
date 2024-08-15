033383ebd2a9bbc61e9f03d7f0a26061b11f6a57
danipro777 <daniel1603@mesoamericana.edu.gt>
2024-08-14 18:59:52 -0600

    Arreglando archivo index.js

--- routes/index.js
+++ routes/index.js
@@ -6,33 +6,21 @@ const { authenticateToken } = require('../middlewares/authMiddleware');
 
 // Importar el controlador de usuarios
 const residencialesController = require('../controllers/residencialesController');
-
 const telefonosController = require('../controllers/telefonosController');
 const controllerRecarga = require('../controllers/recargaController');
 const usuariosController = require('../controllers/usuariosController');
 const controllerEmpleado = require('../controllers/empleadoController');
 const clientesController = require('../controllers/clientesController');
 
-// Definir las rutas
 module.exports = (app) => {
     // Rutas Públicas (No protegidas por el middleware)
     router.post('/login', usuariosController.login);
 
     // Aplicar el middleware de autenticación a partir de aquí
+    // <---------------------------------------------------------------------------------------------------------------------------------------->
     router.use(authenticateToken);
 
-   //ruta GET Residencial
-   router.get('/residenciales/get', residencialesController.find);
-   //ruta GETby Id Residencial
-   router.get('/residenciales/:idResidencia', residencialesController.findById);
-   //ruta POST residencial
-   router.post('/residenciales/create', residencialesController.create);
-   //ruta PUT residencial
-   router.put('/residenciales/update', residencialesController.update);
-   //ruta DELETE residencial
-   router.delete('/residenciales/delete/:idResidencia', residencialesController.delete)
-    // Rutas Públicas (No protegidas por el middleware)
-    router.post('/login', usuariosController.login);
+    // <-------------------------- TABLAS FUERTES -------------------------->
 
     // Rutas de Usuarios (Protegidas)
     router.get('/usuarios', usuariosController.find);
@@ -41,14 +29,41 @@ module.exports = (app) => {
     router.put('/usuarios/:id', usuariosController.update);
     router.delete('/usuarios/:id', usuariosController.delete);
 
-    // rutas de recargas
+    // Rutas de Clientes (Protegidas)
+    router.get('/clientes', clientesController.find);
+    router.get('/clientes/:id', clientesController.findById);
+    router.post('/clientes', clientesController.create);
+    router.put('/clientes/:id', clientesController.update);
+    router.delete('/clientes/:id', clientesController.delete);
+
+    // Rutas de Recargas (Protegidas)
     router.get('/recargas/getAll', controllerRecarga.getAllRecargas);
     router.get('/recargas/getId/:idRecarga', controllerRecarga.getRecargaById);
     router.post('/recargas/create', controllerRecarga.createRecarga);
     router.put('/recargas/update/:idRecarga', controllerRecarga.updateRecarga);
     router.delete('/recargas/delete/:idRecarga', controllerRecarga.deleteRecarga);
 
-    // rutas de empleados
+    // Rutas de Telefonos (Protegidas)
+    router.get('/telefonos/get', telefonosController.find);
+    router.get('/telefonos/:idTelefono', telefonosController.findById);
+    router.post('/telefonos/create', telefonosController.create);
+    router.put('/telefonos/update/:idTelefono', telefonosController.update);
+    router.delete('/telefonos/delete/:idTelefono', telefonosController.delete);
+
+    //ruta GET Residencial
+    router.get('/residenciales/get', residencialesController.find);
+    //ruta GETby Id Residencial
+    router.get('/residenciales/:idResidencia', residencialesController.findById);
+    //ruta POST residencial
+    router.post('/residenciales/create', residencialesController.create);
+    //ruta PUT residencial
+    router.put('/residenciales/update', residencialesController.update);
+    //ruta DELETE residencial
+    router.delete('/residenciales/delete/:idResidencia', residencialesController.delete);
+
+    // <-------------------------- TABLAS DEBILES -------------------------->
+
+    // rutas de empleados (Protegidas)
     router.get('/empleados/getAll', controllerEmpleado.getAllEmpleados); // obtener todos
     router.get('/empleados/getId/:idEmpleado', controllerEmpleado.getEmpleadoById); // obtener por id de empleado
     router.get('/empleados/getName/:nombre', controllerEmpleado.getEmpleadoByName); // obtener por nombre de empleado
@@ -57,20 +72,6 @@ module.exports = (app) => {
     router.put('/empleados/updateByName/:nombre', controllerEmpleado.updateEmpleadoByName); // actualizar por nombre de empleado
     router.delete('/empleados/delete/:idEmpleado', controllerEmpleado.deleteEmpleadoWithID); // eliminar por id de empleado
     router.delete('/empleados/deleteByName/:nombre', controllerEmpleado.deleteEmpleadoByName); // eliminar por nombre de empleado
-
-    // Rutas de Telefonos
-    router.get('/telefonos/get', telefonosController.find);
-    router.get('/telefonos/:idTelefono', telefonosController.findById);
-    router.post('/telefonos/create', telefonosController.create);
-    router.put('/telefonos/update/:idTelefono', telefonosController.update);
-    router.delete('/telefonos/delete/:idTelefono', telefonosController.delete);
-
-    // Rutas de Clientes (Protegidas)
-    router.get('/clientes', clientesController.find);
-    router.get('/clientes/:id', clientesController.findById);
-    router.post('/clientes', clientesController.create);
-    router.put('/clientes/:id', clientesController.update);
-    router.delete('/clientes/:id', clientesController.delete);
 
     // Utilizar el enrutador en la aplicación
     app.use('/', router);
