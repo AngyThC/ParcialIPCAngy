d3fc0ee668912925bc269eadeef6f9241d430c33
PawelReynoso24gt <pawelreynoso@gmail.com>
2024-08-14 18:47:43 -0600

    arreglado de rutas

--- routes/index.js
+++ routes/index.js
@@ -17,30 +17,6 @@ module.exports = (app) => {
     // Rutas Públicas (No protegidas por el middleware)
     router.post('/login', usuariosController.login);
 
-    // Rutas de Telefonos
-    router.get('/telefonos/get', telefonosController.find);
-    router.get('/telefonos/:idTelefono', telefonosController.findById);
-    router.post('/telefonos/create', telefonosController.create);
-    router.put('/telefonos/update/:idTelefono', telefonosController.update);
-    router.delete('/telefonos/delete/:idTelefono', telefonosController.delete);
-
-    // rutas de recargas
-    router.get('/recargas/getAll', controllerRecarga.getAllRecargas);
-    router.get('/recargas/getId/:idRecarga', controllerRecarga.getRecargaById);
-    router.post('/recargas/create', controllerRecarga.createRecarga);
-    router.put('/recargas/update/:idRecarga', controllerRecarga.updateRecarga);
-    router.delete('/recargas/delete/:idRecarga', controllerRecarga.deleteRecarga);
-
-    // rutas de empleados
-    router.get('/empleados/getAll', controllerEmpleado.getAllEmpleados); // obtener todos
-    router.get('/empleados/getId/:idEmpleado', controllerEmpleado.getEmpleadoById); // obtener por id de empleado
-    router.get('/empleados/getName/:nombre', controllerEmpleado.getEmpleadoByName); // obtener por nombre de empleado
-    router.post('/empleados/createIDE', controllerEmpleado.createEmpleadoWithID); // crear con el id de usuario
-    router.put('/empleados/update/:idEmpleado', controllerEmpleado.updateEmpleadoWithID); // actualizar por id de empleado
-    router.put('/empleados/updateByName/:nombre', controllerEmpleado.updateEmpleadoByName); // actualizar por nombre de empleado
-    router.delete('/empleados/delete/:idEmpleado', controllerEmpleado.deleteEmpleadoWithID); // eliminar por id de empleado
-    router.delete('/empleados/deleteByName/:nombre', controllerEmpleado.deleteEmpleadoByName); // eliminar por nombre de empleado
-
     // Aplicar el middleware de autenticación a partir de aquí
     router.use(authenticateToken);
 
@@ -63,6 +39,30 @@ module.exports = (app) => {
     router.get('/usuarios/:id', usuariosController.findById);
     router.put('/usuarios/:id', usuariosController.update);
     router.delete('/usuarios/:id', usuariosController.delete);
+
+    // rutas de recargas
+    router.get('/recargas/getAll', controllerRecarga.getAllRecargas);
+    router.get('/recargas/getId/:idRecarga', controllerRecarga.getRecargaById);
+    router.post('/recargas/create', controllerRecarga.createRecarga);
+    router.put('/recargas/update/:idRecarga', controllerRecarga.updateRecarga);
+    router.delete('/recargas/delete/:idRecarga', controllerRecarga.deleteRecarga);
+
+    // rutas de empleados
+    router.get('/empleados/getAll', controllerEmpleado.getAllEmpleados); // obtener todos
+    router.get('/empleados/getId/:idEmpleado', controllerEmpleado.getEmpleadoById); // obtener por id de empleado
+    router.get('/empleados/getName/:nombre', controllerEmpleado.getEmpleadoByName); // obtener por nombre de empleado
+    router.post('/empleados/createIDE', controllerEmpleado.createEmpleadoWithID); // crear con el id de usuario
+    router.put('/empleados/update/:idEmpleado', controllerEmpleado.updateEmpleadoWithID); // actualizar por id de empleado
+    router.put('/empleados/updateByName/:nombre', controllerEmpleado.updateEmpleadoByName); // actualizar por nombre de empleado
+    router.delete('/empleados/delete/:idEmpleado', controllerEmpleado.deleteEmpleadoWithID); // eliminar por id de empleado
+    router.delete('/empleados/deleteByName/:nombre', controllerEmpleado.deleteEmpleadoByName); // eliminar por nombre de empleado
+
+    // Rutas de Telefonos
+    router.get('/telefonos/get', telefonosController.find);
+    router.get('/telefonos/:idTelefono', telefonosController.findById);
+    router.post('/telefonos/create', telefonosController.create);
+    router.put('/telefonos/update/:idTelefono', telefonosController.update);
+    router.delete('/telefonos/delete/:idTelefono', telefonosController.delete);
 
     // Utilizar el enrutador en la aplicación
     app.use('/', router);
